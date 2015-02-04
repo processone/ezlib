93528177ff300bb3ab444e61891576fc7a38c671
Christophe Romain <christophe.romain@process-one.net>
2015-02-04 13:39:27 +0100

    Update copyright up to 2015

--- src/ezlib.erl
+++ src/ezlib.erl
@@ -5,7 +5,7 @@
 %%% Created : 19 Jan 2006 by Alexey Shchepin <alexey@process-one.net>
 %%%
 %%%
-%%% ezlib, Copyright (C) 2002-2013   ProcessOne
+%%% ezlib, Copyright (C) 2002-2015   ProcessOne
 %%%
 %%% This program is free software; you can redistribute it and/or
 %%% modify it under the terms of the GNU General Public License as
